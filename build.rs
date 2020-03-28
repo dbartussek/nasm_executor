e86358b1ac1aae90dbc29d6b2436720d46002fbe
Dario Bartussek <d.bartussek@gmail.com>
2020-03-28 14:57:52 +0100

    Formatted code

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 use nasm_rs::*;
 
 pub fn main() {
-    Build::new().file("src/assembly.asm").compile("assembly");
+    Build::new()
+        .file("src/assembly.asm")
+        .compile("assembly");
 }
